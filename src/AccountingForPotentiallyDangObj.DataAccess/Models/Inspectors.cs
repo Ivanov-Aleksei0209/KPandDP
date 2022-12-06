bb28b79b7f2af7e4489be1f2882d5d5ea88251f0
ivanovaleksei <ivanov.aleksei020979@gmail.com>
2022-12-06 23:12:22 +0300

    Test GetAll added

--- src/AccountingForPotentiallyDangObj.DataAccess/Models/Inspectors.cs
+++ src/AccountingForPotentiallyDangObj.DataAccess/Models/Inspectors.cs
@@ -1,8 +1,11 @@
-﻿namespace AccountingForPotentiallyDangObj.DataAccess.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountingForPotentiallyDangObj.DataAccess.Models
 {
     public class Inspectors
         // сделать рефакторинг кода на единственное число
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
     }
---
+++ test/AccountingForPotentiallyDangObj.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,35 @@
+using AccountingForPotentiallyDangObj.DataAccess.AfPdoRepository;
+using AccountingForPotentiallyDangObj.DataAccess.EF;
+using AccountingForPotentiallyDangObj.DataAccess.Models;
+
+namespace AccountingForPotentiallyDangObj.IntegrationTests
+{
+    public class Tests
+    {
+        private AfPdoDbContext _tmContext;
+        private AfPdoRepository<Inspectors> _entityRepository;
+        [OneTimeSetUp]
+        public void OneTimeSetUp() 
+        {
+            _tmContext = new AfPdoDbContext("Data Source=ALIVAN\\SQLEXPRESS;Initial Catalog=AccountingForPotentiallyDangObj.DataBase;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False");
+        }
+        [SetUp]
+        public void InitialiseTestsEntities()
+        {
+            _entityRepository = new AfPdoRepository<Inspectors>(_tmContext);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Arrange
+
+            // Act
+            var entities = _entityRepository.GetAllAsync().Result;
+
+            // Assert
+            Assert.Pass();
+        }
+
+    }
+}
