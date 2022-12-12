7a5fa868ef7ffe210982a96d5f1a7043523dd499
ivanovaleksei <ivanov.aleksei020979@gmail.com>
2022-12-12 21:52:38 +0300

    First Unit created

--- src/AccountingForPotentiallyDangObj.DataAccess/Models/TechnicalSpecification.cs
+++ src/AccountingForPotentiallyDangObj.DataAccess/Models/TechnicalSpecification.cs
@@ -13,5 +13,16 @@ namespace AccountingForPotentiallyDangObj.DataAccess.Models
         public double ArrowDeparture { get; set; }
         public double Speed { get; set; }
         public int NumberOfStops { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is TechnicalSpecification technicalSpecification)
+            {
+                return Id == technicalSpecification.Id;
+            }
+            return false;                
+        }
+
+
     }
 }
--- test/AccountingForPotentiallyDangObj.IntegrationTests/AfPdoRepositoryTechnicalSpecificationTests.cs
+++ test/AccountingForPotentiallyDangObj.IntegrationTests/AfPdoRepositoryTechnicalSpecificationTests.cs
@@ -1,6 +1,7 @@
 using AccountingForPotentiallyDangObj.DataAccess.AfPdoRepository;
 using AccountingForPotentiallyDangObj.DataAccess.EF;
 using AccountingForPotentiallyDangObj.DataAccess.Models;
+using System.Collections;
 
 namespace AccountingForPotentiallyDangObj.IntegrationTests
 {
@@ -23,14 +24,43 @@ namespace AccountingForPotentiallyDangObj.IntegrationTests
         [Test]
         public void Test1()
         {
+            int val = 300;
+            double dob = (double)val;
+
             // Arrange
+            //int[] array = {1, 2, 4};
+            //int[] ar = new int[3];
+            //Array ar1 = ar;
+
+
+
+            //ArrayList array List = new ArrayList();
+            TechnicalSpecification expectedObj1 = new TechnicalSpecification();
+            expectedObj1.Id = 1;
+            expectedObj1.Capacity = 25;
+            expectedObj1.ArrowDeparture = 18.5;
+            expectedObj1.Speed = 0;
+            expectedObj1.NumberOfStops = 0;
+
+            TechnicalSpecification expectedObj2 = new TechnicalSpecification();
+            expectedObj2.Id = 1;
+            expectedObj2.Capacity = 25;
+            expectedObj2.ArrowDeparture = 18.5;
+            expectedObj2.Speed = 0;
+            expectedObj2.NumberOfStops = 0;
+
+            List<TechnicalSpecification> expected = new List<TechnicalSpecification>()
+            {
+                expectedObj1,
+                expectedObj2
+            };
 
             // Act
             var entitiesTechnicalSpecification = _technicalSpecificationRepository.GetAllAsync().Result;
 
             // Assert
-            Assert.Pass();
+            Assert.AreEqual(expected, entitiesTechnicalSpecification);
         }
 
     }
-}
+} 
