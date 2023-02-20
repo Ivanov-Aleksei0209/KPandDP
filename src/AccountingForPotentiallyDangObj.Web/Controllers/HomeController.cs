4bc59bffc06db7fa0c213620db748b1cc46da200
ivanovaleksei <ivanov.aleksei020979@gmail.com>
2023-02-20 22:17:02 +0300

    First step. 2 errors((

--- src/AccountingForPotentiallyDangObj.Web/Controllers/HomeController.cs
+++ src/AccountingForPotentiallyDangObj.Web/Controllers/HomeController.cs
@@ -1,21 +1,25 @@
 ﻿using AccountingForPotentiallyDangObj.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using AccountingForPotentiallyDangObj.DataAccess.Interfaces;
+using AccountingForPotentiallyDangObj.DataAccess.Models;
 
 namespace AccountingForPotentiallyDangObj.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        //private readonly ILogger<HomeController> _logger;
+        public readonly IRepository<Inspector> _repositoryInspector;
+        public HomeController(IRepository<Inspector> repositoryInspector)
         {
-            _logger = logger;
+            _repositoryInspector = repositoryInspector;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var inspectors = _repositoryInspector.GetAll().AsEnumerable();
+
+            return View(inspectors);
         }
 
         public IActionResult Privacy()
---
+++ src/AccountingForPotentiallyDangObj.Web/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using AccountingForPotentiallyDangObj.DataAccess.Infrastructure;
+
+namespace AccountingForPotentiallyDangObj.Web
+{
+    public class Startup
+    {
+        private readonly string _connectionString;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddDataAnnotationsLocalization()
+                .AddViewLocalization();
+            services.AddOptions();
+        }
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new DataAccessModule(_connectionString));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
