d089c3cb259d6c2b28472ba16a3714b5104408f0
ivanovaleksei <ivanov.aleksei020979@gmail.com>
2023-02-21 08:13:41 +0300

    Troubleshooting

--- src/AccountingForPotentiallyDangObj.Web/Startup.cs
+++ src/AccountingForPotentiallyDangObj.Web/Startup.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Autofac.Extensions.DependencyInjection;
-using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
 using AccountingForPotentiallyDangObj.DataAccess.Infrastructure;
 
 namespace AccountingForPotentiallyDangObj.Web
