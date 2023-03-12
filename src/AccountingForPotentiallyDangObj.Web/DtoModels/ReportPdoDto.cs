46fa0dcd928fcaac109857f5903ad11822a75dac
ivanovaleksei <ivanov.aleksei020979@gmail.com>
2023-03-12 21:19:36 +0300

    cycles for JournalPdo and for TypesPdo in ServicePdo

--- src/AccountingForPotentiallyDangObj.Web/DtoModels/ReportPdoDto.cs
+++ src/AccountingForPotentiallyDangObj.Web/DtoModels/ReportPdoDto.cs
@@ -5,8 +5,14 @@
         public string NamePdo { get; set; }
         public string TypePdoName { get; set; }
         public int Quantity { get; set; }
-
-
+        public int QuantityOld { get; set; }
+        public int QuantityJournalPdo { get; set; }
+        public int QuantityJournalPdoOld { get; set; }
+        public double PercentOld { get; set; }
+        public double PercentJournalPdoOld { get; set; }
+        public int SumQuantityJournalPdo { get; set; }
+        public string NameJournal { get; set; }
+        public int JournalNumber { get; set; }
 
     }
 }
--- src/AccountingForPotentiallyDangObj.Web/Services/PdoService.cs
+++ src/AccountingForPotentiallyDangObj.Web/Services/PdoService.cs
@@ -15,7 +15,7 @@ namespace AccountingForPotentiallyDangObj.Web.Services
         private readonly IRepository<Subject> _repositorySubject;
         private readonly IMapperConfig _mapperConfig;
 
-        public PdoService(IRepository<Pdo> repositoryPdo, IRepository<JournalPdo> repositoryJournalPdo, IRepository<TypeOfPdo> repositoryTypeOfPdo,IRepository<TechnicalConditional> repositoryTechnicalConditional, 
+        public PdoService(IRepository<Pdo> repositoryPdo, IRepository<JournalPdo> repositoryJournalPdo, IRepository<TypeOfPdo> repositoryTypeOfPdo, IRepository<TechnicalConditional> repositoryTechnicalConditional,
             IRepository<Inspector> repositoryInspector, IRepository<Subject> repositorySubject, IMapperConfig mapperConfig)
         {
             _repositoryPdo = repositoryPdo;
@@ -27,7 +27,7 @@ namespace AccountingForPotentiallyDangObj.Web.Services
             _mapperConfig = mapperConfig;
         }
 
-        public IEnumerable<PdoDto> GetAllAsync() 
+        public IEnumerable<PdoDto> GetAllAsync()
         {
             var models = _repositoryPdo.GetAll();
             var modelsJournalPdo = _repositoryJournalPdo.GetAll();
@@ -36,7 +36,7 @@ namespace AccountingForPotentiallyDangObj.Web.Services
             var modelsInspector = _repositoryInspector.GetAll();
             var modelsSubject = _repositorySubject.GetAll();
             var modelsDto = _mapperConfig.Mapper.Map<IEnumerable<PdoDto>>(models);
-            foreach (var modelDto in modelsDto) 
+            foreach (var modelDto in modelsDto)
             {
                 var journalPdoById = modelsJournalPdo.Where(x => x.Id == modelDto.JournalPdoId).FirstOrDefault();
                 var typeOfPdoById = modelsTypeOfPdo.Where(x => x.Id == modelDto.TypeId).FirstOrDefault();
@@ -44,7 +44,7 @@ namespace AccountingForPotentiallyDangObj.Web.Services
                 var inspectorById = modelsInspector.Where(x => x.Id == modelDto.InspectorId).FirstOrDefault();
                 var subjectById = modelsSubject.Where(x => x.Id == modelDto.SubjectId).FirstOrDefault();
 
-                //modelDto.JournalPdoId = journalPdoById.JournalNumber;
+                modelDto.JournalPdoId = journalPdoById.JournalNumber;
                 modelDto.TypeName = typeOfPdoById.Abb;
                 modelDto.TechnicalConditionalName = techCondModelById.Name;
                 modelDto.InspectorName = inspectorById.Name;
@@ -57,43 +57,90 @@ namespace AccountingForPotentiallyDangObj.Web.Services
         {
             var pdoModels = _repositoryPdo.GetAll().ToList();
             var typesPdoModels = _repositoryTypeOfPdo.GetAll().ToList();
-            
+            var journalsPdoModels = _repositoryJournalPdo.GetAll().ToList();
+
             var typesPdoId = new List<int>();
             typesPdoId = _repositoryTypeOfPdo.GetAll().Select(x => x.Id).ToList();
 
-            //var tempCollectionModels = new List<Pdo>();
+            var journalsPdoId = new List<int>();
+            journalsPdoId = _repositoryJournalPdo.GetAll().Select(x => x.Id).ToList();
+
 
             var reportsPdoDto = new List<ReportPdoDto>();
+
+            for (var j = 0; j < journalsPdoId.Count(); j++)
+            {
+                var journalPdoId = journalsPdoId[j];
+
+
+                var tempCollectionModels = new List<Pdo>();
+                var tempCollectionOldModels = new List<Pdo>();
+                var reportPdoModel = new ReportPdoDto();
+
+                for (var i = 0; i < pdoModels.Count(); i++)
+                {
+                    var pdoModel = pdoModels[i];
+                    if (pdoModel.JournalPdoId == journalPdoId && pdoModel.WithdrawalFromRegistration == null)
+                        tempCollectionModels.Add(pdoModel);
+                    if (pdoModel.JournalPdoId == journalPdoId && DateTime.Now.Year - pdoModel.YearOfManufacture > pdoModel.ServiceLife && pdoModel.WithdrawalFromRegistration == null)
+                        tempCollectionOldModels.Add(pdoModel);
+                }
+
+
+                reportPdoModel.QuantityJournalPdo = tempCollectionModels.Count();
+                reportPdoModel.QuantityJournalPdoOld = tempCollectionOldModels.Count();
+
+                if (tempCollectionModels.Count() != 0)
+                {
+                    double percentJournalPdoOld = (double)tempCollectionOldModels.Count() / (double)tempCollectionModels.Count() * 100;
+                    reportPdoModel.PercentJournalPdoOld = Math.Round(percentJournalPdoOld, 1);
+                }
+                else { reportPdoModel.PercentJournalPdoOld = 0; }
+
+                var journalPdoModel = journalsPdoModels[j];
+                reportPdoModel.NameJournal = journalPdoModel.Name;
+                reportPdoModel.JournalNumber = journalPdoModel.JournalNumber;
+
+                reportsPdoDto.Add(reportPdoModel);
+                reportPdoModel.QuantityJournalPdo = reportsPdoDto.Sum(x => x.Quantity);
+
+            }
 
             for (var k = 0; k < typesPdoId.Count(); k++)
             {
                 var typePdoId = typesPdoId[k];
                 var tempCollectionModels = new List<Pdo>();
+                var tempCollectionOldModels = new List<Pdo>();
+                var reportPdoModel = new ReportPdoDto();
 
                 for (var i = 0; i < pdoModels.Count(); i++)
                 {
                     var pdoModel = pdoModels[i];
                     if (pdoModel.TypeId == typePdoId && pdoModel.WithdrawalFromRegistration == null)
-                    tempCollectionModels.Add(pdoModel);
+                        tempCollectionModels.Add(pdoModel);
+                    if (pdoModel.TypeId == typePdoId && DateTime.Now.Year - pdoModel.YearOfManufacture > pdoModel.ServiceLife && pdoModel.WithdrawalFromRegistration == null)
+                        tempCollectionOldModels.Add(pdoModel);
                 }
-                var reportPdoModel = new ReportPdoDto();
-                
-                if (tempCollectionModels.Count() != null)
+
+                //if (tempCollectionModels.Count() != 0)
+                //{
+                reportPdoModel.Quantity = tempCollectionModels.Count();
+                reportPdoModel.QuantityOld = tempCollectionOldModels.Count();
+                if (tempCollectionModels.Count() != 0)
                 {
-                    reportPdoModel.Quantity = tempCollectionModels.Count();
-                    reportsPdoDto.Add(reportPdoModel);
+                    double percentOld = (double)tempCollectionOldModels.Count() / (double)tempCollectionModels.Count() * 100;
+                    reportPdoModel.PercentOld = Math.Round(percentOld, 1);
                 }
+                else { reportPdoModel.PercentOld = 0; }
+
+                reportsPdoDto.Add(reportPdoModel);
+                //}
                 var typePdoModel = typesPdoModels[k];
                 reportPdoModel.TypePdoName = typePdoModel.Name;
 
-                //reportPdoModel.TypePdoName = typesPdoModels.Where(x => x.Id == typePdoId).Select(x => x.Name);
-                //reportsPdoDto.Add(reportPdoModel);
-            }            
+                reportPdoModel.SumQuantityJournalPdo = reportsPdoDto.Sum(x => x.Quantity);
+            }
             return reportsPdoDto;
         }
-
-
-        
-
     }
 }
